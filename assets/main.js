03c51e7e0fe360d49571d0e6f8a72488b2a0f292
Manoj Nakum <uid.manojn@rishabhsoft.com>
2017-08-29 11:52:11 +0530

    adding all files

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(document).on('click', '#register', function() {
+    	$('.form-block').hide();
+    	$('.link-block .register-link').hide();
+    	$('.link-block .reset-link').hide();
+    	$('.link-block .login-link').show();
+    	$('.link-block .login-link span').html('I already have an account.');
+    	$('.register-form').fadeIn();
+    });
+    $(document).on('click', '#login', function() {
+    	$('.form-block').hide();
+    	$('.link-block .register-link').show();
+    	$('.link-block .reset-link').show();
+    	$('.link-block .login-link').hide();
+    	// $('.link-block .login-link span').html('I already have an account.');
+    	$('.login-form').fadeIn();
+    });
+    $(document).on('click', '#reset-password', function() {
+    	$('.form-block').hide();
+    	$('.link-block .register-link').hide();
+    	$('.link-block .reset-link').hide();
+    	$('.link-block .login-link').show();
+    	$('.link-block .login-link span').html('I know my password.');
+    	$('.reset-form').fadeIn();
+    });
+})
